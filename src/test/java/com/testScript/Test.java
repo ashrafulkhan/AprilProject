c4494376ee475006a3bd167b8e1d984646cbbb20
ashrafulkhan <islam49968@yahoo.com>
2018-04-25 22:05:52 -0400

    fifth commit

--- src/test/java/com/testScript/Test.java
+++ src/test/java/com/testScript/Test.java
@@ -1,7 +1,13 @@
 package com.testScript;
 
 public class Test {
+
+    @org.testng.annotations.Test
     public void test(){
         System.out.println("bangladesh");
+    }
+    @org.testng.annotations.Test
+    public void test01(){
+        System.out.println("bangladesh is a beautiful country");
     }
 }
